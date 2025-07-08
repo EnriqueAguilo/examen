5c70fd9a2d4ac1b66641ad8eaa71d9400dec0a1b
EnriqueAguilo <145963497+EnriqueAguilo@users.noreply.github.com>
2025-07-08 09:49:53 +0200

    Update 4. TRIGGERS.sql

--- 4. TRIGGERS.sql
+++ 4. TRIGGERS.sql
@@ -1,3 +1,35 @@
+EXAMEN 2024 A:
+-- Cree un trigger llamado t_asegurar_mismo_tipo_producto_en_pedidos que impida que, a partir de 
+-- ahora, un mismo pedido incluya productos físicos y digitales.
+DELIMITER //
+CREATE OR REPLACE TRIGGER t_asegurar_mismo_tipo_producto_en_pedidos
+BEFORE INSERT ON LineasPedido
+FOR EACH ROW
+BEGIN
+    DECLARE v_tipoProductoNuevo INT;
+    DECLARE v_cantidadTipoProductoOpuesto INT;
+
+    -- Obtener el tipo de producto del nuevo producto
+    SELECT p.tipoProductoId INTO v_tipoProductoNuevo
+    FROM Productos p
+    WHERE p.id = NEW.productoId;
+
+    -- Contar cuántos productos de tipo distinto hay en el mismo pedido
+    SELECT COUNT(*) INTO v_cantidadTipoProductoOpuesto
+    FROM LineasPedido lp
+    JOIN Productos p ON p.id = lp.productoId
+    WHERE lp.pedidoId = NEW.pedidoId AND p.tipoProductoId != v_tipoProductoNuevo;
+
+    -- Si hay productos de tipo distinto error
+    IF v_cantidadTipoProductoOpuesto > 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'En un mismo pedido no puede haber productos físicos y digitales';
+    END IF;
+END;
+//
+
+DELIMITER ;
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 EXAMEN 2024 B:
 
 -- Cree un trigger llamado t_limitar_importe_pedidos_de_menores que impida que, a partir de ahora, 
@@ -32,12 +64,14 @@ BEGIN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Un menor de edad no puede realizar un pedido de más de 500€';
     END IF;
-
 END;
 //
 DELIMITER ;
 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 EXAMEN 2024 C:
+--Cree un trigger llamado p_limitar_unidades_mensuales_de_productos_fisicos que, a partir de este 
+--momento, impida la venta de más de 1000 unidades al mes de cualquier producto físico. 
 
 DELIMITER //
 CREATE OR REPLACE TRIGGER p_limitar_unidades_mensuales_de_productos_fisicos
@@ -55,7 +89,6 @@ BEGIN
 
     -- Solo si el producto es físico, calcular las unidades vendidas este mes
     IF v_tipoNombre = 'Físicos' THEN
-
         SELECT COALESCE(SUM(lp.unidades), 0) INTO v_totalUnidades
         FROM lineaspedido lp
         JOIN pedidos ped ON ped.id = lp.pedidoId
@@ -72,3 +105,4 @@ END;
 //
 DELIMITER ;
 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
